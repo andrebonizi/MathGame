2e9dc928348f714bb19013a6120186648ac656c2
andrebonizi <andrebonizi@gmail.com>
2016-03-14 14:57:50 -0400

    Nada
    Nadinha mesmo

---
+++ desktop/src/com/coco/mathgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.coco.mathgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.coco.mathgame.MathGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MathGame(), config);
+	}
+}
