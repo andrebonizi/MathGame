f384359651361c4243bc6e803ff295e3db69d858
andrebonizi <andrebonizi@gmail.com>
2016-03-15 00:54:30 -0400

    FitScreen added.

--- core/src/Screens/PlayScreen.java
+++ core/src/Screens/PlayScreen.java
@@ -3,17 +3,24 @@ package Screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.coco.mathgame.MathGame;
 
 public class PlayScreen implements Screen{
 
 	private MathGame game;
 	Texture texture;
+	private OrthographicCamera gamecam;
+	private Viewport gamePort;
 	
 	public PlayScreen(MathGame game){
 		this.game = game;
 		texture = new Texture("badlogic.jpg");
+		gamecam = new OrthographicCamera();
+		gamePort = new FitViewport(800, 480, gamecam);
 	}
 	
 	@Override
@@ -24,6 +31,7 @@ public class PlayScreen implements Screen{
 	public void render(float delta) {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		game.batch.setProjectionMatrix(gamecam.combined);
 		game.batch.begin();
 		game.batch.draw(texture, 0, 0);
 		game.batch.end();
@@ -31,8 +39,8 @@ public class PlayScreen implements Screen{
 
 	@Override
 	public void resize(int width, int height) {
-		// TODO Auto-generated method stub
-		// Comment cocos2d-x
+	
+		gamePort.update(width, height);
 	}
 
 	@Override
