258cdbab014a0424c314238789cbfad7b64aa41c
Marcos Siqueira Junior <junior.crvg@gmail.com>
2016-04-20 20:18:14 -0400

    add question

--- core/src/Logic/QuestionsDisposer.java
+++ core/src/Logic/QuestionsDisposer.java
@@ -1,13 +1,17 @@
 package Logic;
 
+import java.util.Random;
+
 public class QuestionsDisposer {
 	private String answer;
+	private Random rand;
 	
 	public QuestionsDisposer(int playerLevel) {
-		
+		rand = new Random();
 	}
 	
 	public Question getQuestion(){
-		return new Question(rand.nextInt(10),rand.nextInt(10));
+		String oper[] = new String[]{"+","-","/","*" };
+		return new Question(rand.nextInt(10),rand.nextInt(10),oper[rand.nextInt(4)]);
 	}
 }
